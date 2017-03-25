d7fd61db90894f7bb7fbe7cb7f012219a60e17e4
Timothy Cox <cobalt111@hotmail.com>
2017-03-24 20:06:35 -0400

    added post functionality

---
+++ app/src/main/java/com/example/tim/lostnfound/NewAnimal.java
@@ -0,0 +1,90 @@
+package com.example.tim.lostnfound;
+
+import java.util.Date;
+
+/**
+ * Created by Tim on 3/24/2017.
+ */
+
+class NewAnimal {
+
+
+    private String name;
+    private String type;
+    private String location;
+    private String date;
+    private String email;
+    private String phone;
+    private String description;
+    private String color;
+
+
+    NewAnimal(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
--- app/src/main/java/com/example/tim/lostnfound/Post.java
+++ app/src/main/java/com/example/tim/lostnfound/Post.java
@@ -1,13 +1,23 @@
 package com.example.tim.lostnfound;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,15 +27,89 @@ import java.util.Date;
 
 public class Post extends AppCompatActivity {
 
-    @Override
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference ref = database.getReference("server/animals");
+
+    private Button picButton;
+    private Button submitButton;
+    private TextView textView;
+    private EditText editText;
+
+    private String animalID;
+
+    private SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        picButton = (Button) findViewById(R.id.postPictureButton);
+        picButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO add picture functionality
+            }
+        });
 
+        submitButton = (Button) findViewById(R.id.postSubmit);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                animalID = onSubmitAnimal();
 
+                editor.putString("animal_id", animalID);
+                editor.commit();
+            }
+        });
 
     }
+
+
+
+    private String onSubmitAnimal () {
+
+        NewAnimal animal = new NewAnimal();
+        DatabaseReference animalRef = ref.child("animals");
+
+        editText = (EditText) findViewById(R.id.postName);
+        animal.setName(textView.getText().toString());
+
+        editText = (EditText) findViewById(R.id.postColor);
+        animal.setColor(textView.getText().toString());
+
+        editText = (EditText) findViewById(R.id.postDate);
+        animal.setColor(textView.getText().toString());
+
+        editText = (EditText) findViewById(R.id.postEmail);
+        animal.setEmail(textView.getText().toString());
+
+        editText = (EditText) findViewById(R.id.postDescription);
+        animal.setLocation(textView.getText().toString());
+
+        editText = (EditText) findViewById(R.id.postPhone);
+        animal.setPhone(textView.getText().toString());
+
+        //TODO add picture and type
+        //editText = (TextView) findViewById(R.id.postType);
+        //animal.setType(textView.getText().toString());
+
+        animalRef.push().setValue(animal);
+        String postID = animalRef.getKey();
+
+        return postID;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     // for taking the pictures
     static final int REQUEST_IMAGE_CAPTURE = 1;
